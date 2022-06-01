a657d40bbea391519dad5343c2d4da9957564775
Aditya Kumar <kumarad@microsoft.com>
2022-06-01 09:51:53 -0700

    test a bootstrap change

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,8 @@
 git pull origin main;
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
 	echo "This should come here";	
-	source ~/.bash_profile;
+	source ./.bash_profile;
 	echo "This should end here";
 }
 
