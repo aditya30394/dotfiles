c4276c046ac4b1a953c42777e8e6c92dc0e7c13a
Aditya Kumar <kumarad@microsoft.com>
2022-06-03 04:08:42 +0000

    some configuration changes to dotfiles

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git pull origin main;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "install.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+doIt;
+
+unset doIt;
